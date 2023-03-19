24d4ad103d404a6da3a9bc3cb4a95576cacb1b94
Jayant Anand Jha <jayantanandjha4186@gmail.com>
2023-03-19 05:47:25 +0530

    First Commit

---
+++ data_preprocessing.py
@@ -0,0 +1,50 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression 
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+heart_data = pd.read_csv('heart.csv')
+I = heart_data.drop(columns = 'target', axis=1)
+O = heart_data['target']
+I_train, I_test, O_train, O_test = train_test_split(I, O, test_size=0.2, stratify=O, random_state=2)
+print(I.shape, I_train.shape, I_test.shape)
+model = RandomForestClassifier(max_depth=None, random_state=0)
+model.fit(I_train, O_train)
+I_train_prediction = model.predict(I_train)
+I_test_prediction = model.predict(I_test)
+training_data_accuracy = accuracy_score(I_train_prediction, O_train)*100
+test_data_accuracy = accuracy_score(I_test_prediction, O_test)*100
+print('Accuracy on Training data for Random Forest : ', training_data_accuracy)
+print('Accuracy on Test data for Random Forest : ', test_data_accuracy)
+import pickle
+pickle.dump(model,open('model_random_forest.pkl','wb'))
+model = LogisticRegression()
+model.fit(I_train, O_train)
+I_train_prediction = model.predict(I_train)
+I_test_prediction = model.predict(I_test)
+training_data_accuracy = accuracy_score(I_train_prediction, O_train)*100
+test_data_accuracy = accuracy_score(I_test_prediction, O_test)*100
+print('Accuracy on Training data for Logistic Regression : ', training_data_accuracy)
+print('Accuracy on Test data for Logistic Regression : ', test_data_accuracy)
+pickle.dump(model,open('model_logistic_regression.pkl','wb'))
+model = svm.SVC()
+model.fit(I_train, O_train)
+I_train_prediction = model.predict(I_train)
+I_test_prediction = model.predict(I_test)
+training_data_accuracy = accuracy_score(I_train_prediction, O_train)*100
+test_data_accuracy = accuracy_score(I_test_prediction, O_test)*100
+print('Accuracy on Training data for SVM : ', training_data_accuracy)
+print('Accuracy on Test data for SVM : ', test_data_accuracy)
+pickle.dump(model,open('model_svm.pkl','wb'))
+model = DecisionTreeClassifier()
+model.fit(I_train, O_train)
+I_train_prediction = model.predict(I_train)
+I_test_prediction = model.predict(I_test)
+training_data_accuracy = accuracy_score(I_train_prediction, O_train)*100
+test_data_accuracy = accuracy_score(I_test_prediction, O_test)*100
+print('Accuracy on Training data for Decision Tree : ', training_data_accuracy)
+print('Accuracy on Test data for Decision Tree : ', test_data_accuracy)
+pickle.dump(model,open('model_decision_tree.pkl','wb'))
---
+++ flask_api.py
@@ -0,0 +1,29 @@
+from flask import Flask,request,jsonify
+import numpy as np
+import pickle
+model = pickle.load(open('model_svm.pkl','rb'))
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return "Starting"
+@app.route('/predict/',methods=['POST'])
+def predict():
+    age = float(request.form.get('age'))
+    sex = float(request.form.get('sex'))
+    cp = float(request.form.get('cp'))
+    trestbps = float(request.form.get('trestbps'))
+    chol = float(request.form.get('chol'))
+    fbs = float(request.form.get('fbs'))
+    restecg = float(request.form.get('restecg'))
+    thalach = float(request.form.get('thalach'))
+    exang = float(request.form.get('exang'))
+    oldpeak = float(request.form.get('oldpeak'))
+    slope = float(request.form.get('slope'))
+    ca = float(request.form.get('ca'))
+    thal = float(request.form.get('thal'))
+    input_query = np.array([[age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal]])
+    input_data_reshaped = input_query.reshape(1,-1)
+    result = model.predict(input_data_reshaped)
+    return jsonify({'heart_disease':str(result)})
+if __name__ == '__main__':
+    app.run(debug=True)
